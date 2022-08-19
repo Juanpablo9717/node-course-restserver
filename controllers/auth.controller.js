ac6a6e991ff45fcf1a3e6b7478940418dc032f69
Juanpablo9717 <juanpablo9717@gmail.com>
2022-08-19 18:06:50 -0500

    Validate Google token

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,33 +3,33 @@ const bcryptjs = require("bcryptjs");
 
 const User = require("../models/users.schema");
 const { generateJWT } = require("../helpers/generateJWT");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async (req, res = response) => {
   const { email, password } = req.body;
 
   try {
-
-    // Check if email exists 
+    // Check if email exists
     const user = await User.findOne({ email });
     if (!user) {
-        return res.status(400).json({ 
-            msg: "Email or password invalid - email" 
-        });
+      return res.status(400).json({
+        msg: "Email or password invalid - email",
+      });
     }
 
     // Check if user is active
     if (!user.state) {
-        return res.status(400).json({ 
-            msg: "Email or password invalid - status false" 
-        });
+      return res.status(400).json({
+        msg: "Email or password invalid - status false",
+      });
     }
 
     // Check password
     const validPassword = bcryptjs.compareSync(password, user.password);
     if (!validPassword) {
-        return res.status(400).json({ 
-            msg: "Email or password invalid - password" 
-        });
+      return res.status(400).json({
+        msg: "Email or password invalid - password",
+      });
     }
 
     // Generate JWT
@@ -37,9 +37,8 @@ const login = async (req, res = response) => {
 
     res.json({
       user,
-      token
+      token,
     });
-
   } catch (error) {
     console.log(error);
     res.status(500).json({
@@ -49,17 +48,25 @@ const login = async (req, res = response) => {
   }
 };
 
-const googleSignIn = (req, res = response) => {
-
+const googleSignIn = async (req, res = response) => {
   const { id_token } = req.body;
 
-  res.json({
-    msh: 'All is well',
-    id_token
-  })
+  try {
+    const { name, email, picture} = await googleVerify(id_token);
+
+    res.json({
+      msh: "All is well",
+      id_token,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      ok: false,
+      msg: "Token failed to authorize.",
+    });
+  }
 };
 
 module.exports = {
   login,
-  googleSignIn
+  googleSignIn,
 };
